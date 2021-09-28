0859a7153fbf209858651f8ceb02de8b6d6ccf3a
Abdulhamid Ibrahim <71563206+Hamid8542@users.noreply.github.com>
2021-09-27 23:33:19 -0500

    Delete tips.js

--- tips.js
+++
@@ -1,9 +0,0 @@
-
-        function calc() {
-            var bill = document.getElementById('bill').value;
-            var tip = bill * .15;
-            var total = bill*1 + tip;
-    
-            document.getElementById("tip").innerHTML= "$"+(tip).toFixed(2);
-            document.getElementById("total").innerHTML= "$"+(total).toFixed(2);         
-        }
