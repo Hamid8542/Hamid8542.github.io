3dd985901e20e2d632029f029f90e07b7bec757c
Abdulhamid Ibrahim <71563206+Hamid8542@users.noreply.github.com>
2022-03-30 18:38:45 -0500

    Create calculator.js

---
+++ js/calculator.js
@@ -0,0 +1,34 @@
+
+function add() {
+    let firstValue = Number(document.getElementById("firstValue").value);
+    let secondValue = Number(document.getElementById("secondValue").value);
+    let result = firstValue + secondValue;
+    document.getElementById("result").innerHTML = result;
+
+    document.getElementById("firstValue").value = "";
+    document.getElementById("secondValue").value = "";
+
+}
+
+function sub() {
+    let firstValue = Number(document.getElementById("firstValue").value);
+    let secondValue = Number(document.getElementById("secondValue").value);
+    let result = firstValue - secondValue;
+    document.getElementById("result").innerHTML = result;
+
+    document.getElementById("firstValue").value = "";
+    document.getElementById("secondValue").value = "";
+
+}
+
+function mul() {
+    let firstValue = Number(document.getElementById("firstValue").value);
+    let secondValue = Number(document.getElementById("secondValue").value);
+    let result = firstValue * secondValue;
+    document.getElementById("result").innerHTML = result;
+
+    document.getElementById("firstValue").value = "";
+    document.getElementById("secondValue").value = "";
+
+}
+
